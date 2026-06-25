327cceb1601252dffafe33470a8e150f965dc64a
Michael <38452654+FreePDF64@users.noreply.github.com>
2026-06-25 17:08:18 +0200

    Neue Version

--- src/OfficeLauncher.pas
+++ src/OfficeLauncher.pas
@@ -729,8 +729,8 @@ begin
   OuterMargin   := Round(BaseOuterMargin   * Scale);
 }
   ButtonHeight := Round(36 * Scale);     // vorher z.B. 30–34 → jetzt etwas kleiner
-  ButtonSpacing := Round(5 * Scale);     // Abstand zwischen Buttons kleiner
-  OuterMargin := Round(7 * Scale);       // Abstand zu den Rändern kleiner
+  ButtonSpacing := Round(2 * Scale);     // Abstand zwischen Buttons kleiner
+  OuterMargin := Round(5 * Scale);       // Abstand zu den Rändern kleiner
 
   BorderIcons := BorderIcons - [biMaximize];
   CoInitialize(nil);
